7ed1a1667d7758b79dc47cf08b1fa757aa5631ff
Frank Farm <frank@frankfinance.app>
2023-06-26 04:05:22 -0400

    feat: add navbar

---
+++ src/lib/components/NavigationBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { cn } from "$lib/utils";
+	import ConnectWallet from "./ConnectWallet.svelte";
+	import LightSwitch from "./ui/light-switch/LightSwitch.svelte";
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<div class="border-b fixed inset-x-0 top-0 z-10 bg-primary-foreground">
+	<div class="flex h-16 items-center px-4">
+		<nav class={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+			<a
+				href="/examples/dashboard"
+				class="text-lg font-medium text-primary transition-colors hover:text-muted-foreground inline-flex items-center gap-1"
+			>
+			frankfinance.app
+			</a>
+		</nav>
+
+		<nav class={cn("flex items-center space-x-4 lg:space-x-6 ml-auto", className)}>
+			<LightSwitch />
+			<ConnectWallet />
+		</nav>
+	</div>
+</div>
