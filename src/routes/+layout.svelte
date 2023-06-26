93742a6a7f9f4825f93032e7f83b1cae281ee8d7
Frank Farm <frank@frankfinance.app>
2023-06-26 04:58:08 -0400

    feat: add proper links to homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import NavigationBar from '$components/NavigationBar.svelte';
 	import { Discord, GitHub, Twitter } from 'svelte-stacks-icons';
 	import { setInitialClassState } from "$components/ui/light-switch/light-switch";
+	import Button from '$components/ui/button/Button.svelte';
 </script>
 
 <svelte:head>
@@ -14,10 +15,16 @@
 
 <slot />
 <div class="border-t fixed inset-x-0 bottom-0 w-full">
-	<div class="w-full flex justify-center bg-primary-foreground p-2 items-center gap-x-2">
-		<p class="text-sm text-muted-foreground">&copy; frankfinance.app -- socials: </p>
-		<Discord />
-		<Twitter />
-		<GitHub />
+	<div class="w-full flex justify-center bg-primary-foreground p-2 items-center">
+		<p class="text-sm text-muted-foreground pr-4">&copy; frankfinance.app -- socials: </p>
+		<Button variant="ghost" href="https://discord.gg/p3cKYHCS" target="_blank" class="w-12 rounded-md">
+			<Discord  />
+		</Button>
+		<Button variant="ghost" href="https://twitter.com/FrankTheFinance" target="_blank" class="w-12 rounded-md">
+			<Twitter />
+		</Button>
+		<Button variant="ghost" href="https://github.com/FrankFinance" target="_blank" class="w-12 rounded-md">
+			<GitHub />
+		</Button>
 	</div>
 </div>
