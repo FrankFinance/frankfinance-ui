49a84ae402135547922885d8923f7e86b100222c
Frank Farm <frank@frankfinance.app>
2023-06-26 04:06:19 -0400

    feat: add token statistics component

---
+++ src/lib/components/StatisticCardGrid.svelte
@@ -0,0 +1,69 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { DollarSign, Banknote, Users } from "lucide-svelte";
+	import { Card, CardHeader, CardTitle, CardContent } from "$components/ui/card";
+</script>
+
+<div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4 p-8 lg:mx-auto" in:fade={{ duration: 100 }}>
+	<Card>
+		<CardHeader
+			class="flex flex-row items-center justify-between space-y-0 pb-2"
+		>
+			<CardTitle class="text-sm font-medium">
+				Token Price
+			</CardTitle>
+			<DollarSign class="h-4 w-4 text-muted-foreground" />
+		</CardHeader>
+		<CardContent>
+			<div class="text-2xl font-bold">$1.20</div>
+			<p class="text-xs text-green-600">
+				+12.0% from yesterday
+			</p>
+		</CardContent>
+	</Card>
+	<Card>
+		<CardHeader
+			class="flex flex-row items-center justify-between space-y-0 pb-2"
+		>
+			<CardTitle class="text-sm font-medium">
+				Market Cap
+			</CardTitle>
+			<Banknote class="h-4 w-4 text-muted-foreground" />
+		</CardHeader>
+		<CardContent>
+			<div class="text-2xl font-bold">$123,017</div>
+		</CardContent>
+	</Card>
+	<Card>
+		<CardHeader
+			class="flex flex-row items-center justify-between space-y-0 pb-2"
+		>
+		<CardTitle class="text-sm font-medium">
+			Total Supply
+		</CardTitle>
+		<Users class="h-4 w-4 text-muted-foreground" />
+		</CardHeader>
+		<CardContent>
+			<div class="text-2xl font-bold">25,000,000</div>
+			<p class="text-xs text-muted-foreground">
+				Fixed - no more will be minted.
+			</p>
+		</CardContent>
+	</Card>
+	<Card>
+		<CardHeader
+			class="flex flex-row items-center justify-between space-y-0 pb-2 md:space-x-44"
+		>
+			<CardTitle class="text-sm font-medium">
+				Emissions
+			</CardTitle>
+			<Users class="h-4 w-4 text-muted-foreground" />
+		</CardHeader>
+		<CardContent>
+			<div class="text-2xl font-bold">15,000</div>
+			<p class="text-xs text-muted-foreground">
+				$FRANK per day - 30d left
+			</p>
+		</CardContent>
+	</Card>
+</div>
