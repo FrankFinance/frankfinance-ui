0387f8978b202019f3fb2f6229e37434c0328c3a
Frank Farm <frank@frankfinance.app>
2023-06-26 04:06:57 -0400

    feat: add index layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from "svelte";
+	import CtaCard from "$components/CtaCard.svelte";
+	import StakingCard from "$components/StakingCard.svelte";
+	import AccordionFaq from "$components/AccordionFaq.svelte";
+	import StatisticCardGrid from "$components/StatisticCardGrid.svelte";
+
+	let visible = true,
+		onLoad = false;
+
+	onMount(() => {
+		setTimeout(() => onLoad = true, 10)
+	})
+</script>
+
+{#if onLoad}
+	<div class="flex flex-col mt-20 mb-8">
+		<div class="flex gap-14 lg:gap-24 justify-center lg:items-end w-full p-8 lg:flex-row flex-col">
+			<CtaCard />
+			<StakingCard />
+		</div>
+		<StatisticCardGrid />
+		<AccordionFaq />
+	</div>
+{/if}
